2f05071b79bdeffe630ca3b12565ccb9f790a25d
HariPC_R542 <shriharipardhikar123@gmail.com>
2022-06-16 11:36:38 +0530

    initiate

--- app.js
+++ app.js
@@ -1,20 +1,66 @@
 //importing all dependancies
-import express from 'express'; //main dependancy for making req res for server
-import mongoose from 'mongoose';
-import mongouri from 'privateconstant.js'
+//import mongouri from 'privateconstant.js'
+const mongoose =require( 'mongoose');
+const express =require( 'express'); //main dependancy for making req res for server
 
+
+
+//objects
+const app = express();
+const PORT= 4000  
+let mongouri = 'mongodb+srv://harry:QmNWEBXUv8c9uv4@testingcluster.tk7ou.mongodb.net/todo_db?retryWrites=true&w=majority'
 
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 
-//objects
-const app = express();
+app.set('view engine','ejs') //setting view engine ot ejs
 
-mongoose.connect(mongouri,{
-    useNewUrlParser:true,
+//connecting mongodb
+ mongoose.connect(mongouri,{
+    useNewUrlParser:true,       //version of url
     useUnifiedTopology:true   //not assume any structure of data
-}).then((res)=>{
-   
+}).then((res)=>{   
     console.log("connected to db")
+})
+
+function cleanupAndValidate({name,email,password,username}){
+    if(!name||!email||!password)
+    {
+        
+    }
+
+}
+//main route
+app.get('/',(req,res)=>{
+    res.send("welcome to the app")
+})
+app.get('/register',(req,res)=>{
+    res.render('register')
+})
+app.get('/login',(req,res)=>{
+    res.render('login')
+})
+app.post('/register',(req,res)=>{
+
+    const {name,email,password,username}=req.body;
+    cleanupAndValidate({name,email,password,username})//functions for all checks
+
+    try{
+
+    }
+    catch{
+
+    }
+
+    res.send("welcome to the app")
+
+    
+})
+app.post('/login',(req,res)=>{
+    res.send("welcome to the app")
+})
+
+app.listen(PORT,(req,res)=>{
+    console.log("Server is running")
 })
